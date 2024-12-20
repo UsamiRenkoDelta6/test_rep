bdf41bb3a88c08591d1e4d4b51b8b67765a5c21f
U-1_test <reimuhakureiii6@gmail.com>
2024-12-20 17:40:06 +0800

    ++cpp num 4 modificaton. test num 6++

--- Git_test_num1.cpp
+++ Git_test_num1.cpp
@@ -6,5 +6,6 @@ int main(){
 	cout<<"Hi, that 1th git test"<<endl;
 	cout << "---[梶迫迅八] The cradle.. funny chinese song---" << endl;
 	cout << "---изменение 3е, как любовь Христа и ненависть некоторых издателей---" << endl;
+	cout << "---я слежу, 4й вроде. фиксируй---" << endl;
 return 0;
 }
