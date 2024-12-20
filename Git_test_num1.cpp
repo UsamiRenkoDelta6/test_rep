08bb49329b8de2287795ccd3b3bcd63983e9cc12
U-1_test <reimuhakureiii6@gmail.com>
2024-12-20 17:36:08 +0800

    ++After error. test num 5++

---
+++ Git_test_num1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout<<"Hi, that 1th git test"<<endl;
+	cout << "---[梶迫迅八] The cradle.. funny chinese song---" << endl;
+	cout << "---изменение 3е, как любовь Христа и ненависть некоторых издателей---" << endl;
+return 0;
+}
